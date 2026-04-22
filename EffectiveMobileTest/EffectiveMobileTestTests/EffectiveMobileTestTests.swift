02ef97c24e4857679b47ac24eb917dee0231f3f9
Kitolov <nikita.khaziev@mail.ru>
2026-04-22 16:16:00 +0300

    initial commit

---
+++ EffectiveMobileTest/EffectiveMobileTestTests/EffectiveMobileTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestTests.swift
+//  EffectiveMobileTestTests
+//
+//  Created by Nikita on 22.04.2026.
+//
+
+import Testing
+@testable import EffectiveMobileTest
+
+struct EffectiveMobileTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
